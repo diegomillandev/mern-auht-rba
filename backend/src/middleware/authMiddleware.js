885d42fbee1a855fd31ab4e66fe02bb102f89808
diegomillandev <diegomillandev@gmail.com>
2025-10-28 18:55:04 -0500

    feat: implement authentication middleware and add logout and refresh token routes

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+import User from "../models/User.js";
+import { JWT_SECRET } from '../constants/env.js';
+
+export const verifyToken = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: "No token provided" });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
+
+        req.user = user;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: "Token expired or invalid" });
+    }
+};
+
+export const authorizeRoles = (role) => {
+    return (req, res, next) => {
+        const userRole = req.user?.role;
+        if (userRole !== role) {
+            return res.status(403).json({ message: "Access denied" });
+        }
+        next();
+    }
+}
--- backend/src/routes/v1/auth.js
+++ backend/src/routes/v1/auth.js
@@ -5,5 +5,7 @@ const router = Router();
 
 router.post('/register', AuthController.register)
 router.post('/login', AuthController.login)
+router.post('/logout', AuthController.logout)
+router.post('/refresh-token', AuthController.refreshToken)
 
 export default router;
