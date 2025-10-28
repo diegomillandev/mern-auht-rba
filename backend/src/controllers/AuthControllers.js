fa9b128da7ea5f6f98f999d8bb58bb1cddd9abd1
diegomillandev <diegomillandev@gmail.com>
2025-10-28 06:05:26 -0500

    feat: enhance AuthController registration response and improve server.js structure

--- backend/src/controllers/AuthControllers.js
+++ backend/src/controllers/AuthControllers.js
@@ -4,5 +4,7 @@ import catchErrors from "../utils/catchErrors.js";
 export class AuthController {
     static register = catchErrors(async (req, res) => {
         console.log("Register endpoint hit");
+
+        res.status(201).json({ message: "User registered successfully" });
     });
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -8,9 +8,14 @@ connectDB();
 
 const app = express();
 
+// Middleware to parse JSON
+app.use(express.json());
 
+
+// API Routes
 app.use('/api/v1', router)
 
+// Error handling middleware
 app.use(errorHandler)
 
 export default app;
