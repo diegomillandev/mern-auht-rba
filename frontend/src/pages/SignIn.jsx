39e9e7cfc8f2d2c1944584a85534982f46546c7d
diegomillandev <diegomillandev@gmail.com>
2025-10-30 20:34:23 -0500

    feat: enhance SignUp component with form fields and styling

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const SignIn = () => {
   return (
     <>
-      <div className="bg-white text-gray-500 w-96 mx-4 md:p-6 p-4 text-left text-sm rounded-xl shadow-[0px_0px_10px_0px] shadow-black/10">
+      <div className="bg-white text-gray-500 w-96 mx-4 md:p-6 p-4 text-left text-sm rounded-xl border border-slate-300/50 shadow">
         <h2 className="text-2xl font-semibold mb-6 text-center text-gray-800">
           Welcome back
         </h2>
@@ -27,7 +27,7 @@ export const SignIn = () => {
 
           <button
             type="submit"
-            className="w-full bg-indigo-500 py-2.5 rounded text-white mt-4"
+            className="w-full bg-indigo-500 py-2.5 rounded text-white mt-2"
           >
             Log in
           </button>
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,3 +1,59 @@
+import { Link } from "react-router-dom";
 export const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <div className="bg-white text-gray-500 w-96 mx-4 md:p-6 p-4 text-left text-sm rounded-xl border border-slate-300/50 shadow">
+        <h2 className="text-2xl font-semibold mb-6 text-center text-gray-800">
+          Create your account
+        </h2>
+
+        <form className="flex flex-col gap-4 mb-4">
+          <input
+            id="name"
+            className="w-full bg-transparent border border-gray-500/30 outline-none rounded py-2.5 px-4"
+            type="email"
+            placeholder="Enter your name"
+            required
+          />
+          <input
+            id="email"
+            className="w-full bg-transparent border border-gray-500/30 outline-none rounded py-2.5 px-4"
+            type="email"
+            placeholder="Enter your email"
+            required
+          />
+
+          <input
+            id="password"
+            className="w-full bg-transparent border border-gray-500/30 outline-none rounded py-2.5 px-4"
+            type="password"
+            placeholder="Enter your password"
+            required
+          />
+
+          <input
+            id="password_confirmation"
+            className="w-full bg-transparent border border-gray-500/30 outline-none rounded py-2.5 px-4"
+            type="password"
+            placeholder="Confirm your password"
+            required
+          />
+
+          <button
+            type="submit"
+            className="w-full bg-indigo-500 py-2.5 rounded text-white mt-2"
+          >
+            Log in
+          </button>
+        </form>
+
+        <p className="text-center">
+          Already have an account?{" "}
+          <Link to={"/sign-in"} className="text-blue-500 underline">
+            Signin
+          </Link>
+        </p>
+      </div>
+    </>
+  );
 };
